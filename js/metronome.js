43821be810574d7c8cbed80c29c0a779451d4357
swit-ch <stephan.wittwer@gmail.com>
2015-12-27 14:42:47 +0100

    My first changes to scheduling, simple metronome intended. Canvas ignored for now (commented out). Have beatsPerBar, beatUnit, currentBeat, not noteResolution, current16thNote...
    'tempo' currently is for quarter notes, not for 'beatUnit' !
    
    Ah, added a little offset to the first 'nextNoteTime' in play func.
    Now I can hear the first note
    (BTW there is an unused variable in the original code 'startTime',
    was it intended for this purpose?).

--- js/metronome.js
+++ js/metronome.js
@@ -1,7 +1,7 @@
 var audioContext = null;
 var isPlaying = false;      // Are we currently playing?
-var startTime;              // The start time of the entire sequence.
-var current16thNote;        // What note is currently last scheduled?
+// var startTime;              // The start time of the entire sequence.   NOT USED
+// var current16thNote;        // What note is currently last scheduled?
 var tempo = 120.0;          // tempo (in beats per minute)
 var lookahead = 25.0;       // How frequently to call scheduling function 
                             //(in milliseconds)
@@ -9,7 +9,7 @@ var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
                             // This is calculated from lookahead, and overlaps 
                             // with next interval (in case the timer is late)
 var nextNoteTime = 0.0;     // when the next note is due.
-var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
+// var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
 var noteLength = 0.05;      // length of "beep" (in seconds)
 var canvas,                 // the canvas element
     canvasContext;          // canvasContext is the canvas' context 2D
@@ -19,7 +19,13 @@ var notesInQueue = [];      // the notes that have been put into the web audio,
 var timerWorker = null;     // The Web Worker used to fire timer messages
 
 var mainGainNode; // testing
-var gain = 0.5; // tempo and gain defaults here!
+var gain = 0.5; // tempo and gain defaults here
+
+
+var beatsPerBar = 3; // positive integer
+var beatUnit = 1 / 2; 
+var currentBeat;
+
 
 // First, let's shim the requestAnimationFrame API, with a setTimeout fallback
 window.requestAnimFrame = (function(){
@@ -37,27 +43,21 @@ function nextNote() {
     // Advance current note and time by a 16th note...
     var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT 
                                           // tempo value to calculate beat length.
-    nextNoteTime += 0.25 * secondsPerBeat;    // Add beat length to last beat time
+    nextNoteTime += beatUnit * 4 * secondsPerBeat;    // Add beat length to last beat time
 
-    current16thNote++;    // Advance the beat number, wrap to zero
-    if (current16thNote == 16) {
-        current16thNote = 0;
-    }
+    currentBeat++;    // Advance the beat number, wrap to zero
+    if (currentBeat == beatsPerBar) {
+        currentBeat = 0;
+    };
+
 }
-
+// beatNumber is passed in currentBeat
 function scheduleNote( beatNumber, time ) {
     // push the note on the queue, even if we're not playing.
     notesInQueue.push( { note: beatNumber, time: time } );
 
-    if ( (noteResolution==1) && (beatNumber%2))
-        return; // we're not playing non-8th 16th notes
-    if ( (noteResolution==2) && (beatNumber%4))
-        return; // we're not playing non-quarter 8th notes
-
     // create an oscillator
-    var osc = audioContext.createOscillator();
-//     osc.connect( audioContext.destination );
-    
+    var osc = audioContext.createOscillator();    
     var eg = audioContext.createGain();
     
     eg.gain.setValueAtTime(1.0, time);
@@ -65,17 +65,21 @@ function scheduleNote( beatNumber, time ) {
     eg.gain.exponentialRampToValueAtTime(0.001, time + noteLength);
     
     osc.type = [ "sine", "square", "sawtooth", "triangle" ][3];
-    
     osc.connect(eg);
     eg.connect( mainGainNode );
     
-    if (beatNumber % 16 === 0)    // beat 0 == low pitch
-        osc.frequency.value = 880.0;
-    else if (beatNumber % 4 === 0 )    // quarter notes = medium pitch
-        osc.frequency.value = 440.0;
-    else                        // other 16th notes = high pitch
-        osc.frequency.value = 220.0;
-
+    console.log(beatNumber); // correct, but no sound on first zero
+    
+//     if (beatNumber % 16 === 0)    // beat 0 == low pitch
+//         osc.frequency.value = 880.0;
+//     else if (beatNumber % 4 === 0 )    // quarter notes = medium pitch
+//         osc.frequency.value = 440.0;
+//     else                        // other 16th notes = high pitch
+//         osc.frequency.value = 220.0;
+    
+    if (beatNumber === 0)
+    	osc.frequency.value = 880.0;
+    
     osc.start( time );
     osc.stop( time + noteLength );
 }
@@ -84,7 +88,7 @@ function scheduler() {
     // while there are notes that will need to play before the next interval, 
     // schedule them and advance the pointer.
     while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
-        scheduleNote( current16thNote, nextNoteTime );
+        scheduleNote( currentBeat, nextNoteTime );
         nextNote();
     }
 }
@@ -92,9 +96,9 @@ function scheduler() {
 function play() {
     isPlaying = !isPlaying;
 
-    if (isPlaying) { // start playing
-        current16thNote = 0;
-        nextNoteTime = audioContext.currentTime;
+    if (isPlaying) { // start playing        
+        currentBeat = 0;
+        nextNoteTime = audioContext.currentTime + 0.1; // now can hear first beat !
         timerWorker.postMessage("start");
         return "stop";
     } else {
@@ -173,17 +177,14 @@ function init(){
 
     // if we wanted to load audio files, etc., this is where we should do it.
     
-    
-    
     mainGainNode = audioContext.createGain();
     setMainGain(gain); // init
-   
     mainGainNode.connect( audioContext.destination );
 
     window.onorientationchange = resetCanvas;
     window.onresize = resetCanvas;
 
-    requestAnimFrame(draw);    // start the drawing loop.
+//     requestAnimFrame(draw);    // start the drawing loop.
 
     timerWorker = new Worker("js/metronomeworker.js");
 
